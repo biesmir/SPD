3035a7903cc1e3d17fedb8b6c9909c318df88e61
Jakub Cebulski <235773@student.pwr.edu.pl>
2019-03-12 18:44:41 +0100

    klasy Schedule i Job

---
+++ job.py
@@ -0,0 +1,16 @@
+class Job:
+
+    def __init__(self, time):
+        self.time = tuple(time)
+
+    def __str__(self):
+        line = " | "
+        for i in self.time:
+            line += str(i) + " | "
+        return line
+
+    def __repr__(self):
+        line = " | "
+        for i in self.time:
+            line += str(i) + " | "
+        return line
