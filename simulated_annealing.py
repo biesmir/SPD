11498626966851b79bb93ed095d84cb2e0fa7d8d
Jakub Cebulski <235773@student.pwr.edu.pl>
2019-04-06 18:16:28 +0200

    dodanie zapisu kolejnych cmax dla plota

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -8,6 +8,7 @@ def swap(schdl, schedule_length):
     index1 = randint(0, schedule_length - 1)
     index2 = randint(0, schedule_length - 1)
 
+    # sprawdzamy czy nie wylosowało się dwa razy to samo
     while index1 == index2:
         index1 = randint(0, schedule_length - 1)
         index2 = randint(0, schedule_length - 1)
@@ -17,19 +18,21 @@ def swap(schdl, schedule_length):
 
 def annealing(schdl, u=0.98, temp=100, iterations=10):
     schedule_lenght = len(schdl.joblist)
-
+    steps = []
     for i in range(iterations):
         tmp_schdl = schdl.__copy__()
         swap(tmp_schdl, schedule_lenght)
 
         if tmp_schdl.cmax() < schdl.cmax():
             schdl = tmp_schdl
-            print(tmp_schdl.cmax())
+            steps.append(tmp_schdl.cmax())
 
         else:
             prob = math.exp((schdl.cmax() - tmp_schdl.cmax())/temp)
             if random() < prob:
                 schdl = tmp_schdl
-                print(tmp_schdl.cmax())
+                steps.append(tmp_schdl.cmax())
 
         temp = u * temp
+
+    return steps
