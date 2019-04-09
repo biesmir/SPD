2b585b9a05bce461c5b2edd01a23d1b5cb0c3378
Paulina <paulinazawada26@gmail.com>
2019-04-09 18:32:29 +0200

    spraw
    Prawie wszystko

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -31,7 +31,7 @@ def random_insert(schdl, schedule_length):
     schdl.joblist.insert(new_index, job)
 
 
-def annealing(schdl, u=0.98, temp=100, iterations=10, move="swap"):
+def annealing(schdl, u=0.98, temp=100, iterations=10, move="swap", op="normal"):
     schedule_lenght = len(schdl.joblist)
     steps = []
     for i in range(iterations):
@@ -42,15 +42,76 @@ def annealing(schdl, u=0.98, temp=100, iterations=10, move="swap"):
         else:
             swap(tmp_schdl, schedule_lenght)
 
+        # Nowe rozwiazanie lepsze od obecnego
         if tmp_schdl.cmax() < schdl.cmax():
             schdl = tmp_schdl
 
+        # Nowe rozwiazanie gorsze od obecnego
         else:
             prob = math.exp((schdl.cmax() - tmp_schdl.cmax())/temp)
             if random() < prob:
                 schdl = tmp_schdl
 
         steps.append(schdl.cmax())
-        temp = u * temp
+        if op == "alternative":
+            temp = temp * (i+1)/iterations
+        else:
+            temp = u * temp
+
+    return int(schdl.cmax()), steps
+
+def annealing_prob(schdl, u=0.98, temp=100, iterations=10, move="swap", op="normal"):
+    schedule_lenght = len(schdl.joblist)
+    steps = []
+    for i in range(iterations):
+        tmp_schdl = schdl.__copy__()
+
+        if move == "insert":
+            random_insert(tmp_schdl, schedule_lenght)
+        else:
+            swap(tmp_schdl, schedule_lenght)
+        
+        # Odrzucenie prawdopodobienstwa 1 dla nowych lepszych rozwiazan
+        prob = math.exp((schdl.cmax() - tmp_schdl.cmax())/temp)
+        if random() < prob:
+            schdl = tmp_schdl
+
+        steps.append(schdl.cmax())
+        if op == "alternative":
+            temp = temp * (i+1)/iterations
+        else:
+            temp = u * temp
+
+    return int(schdl.cmax()), steps
+
+def annealing_cmax(schdl, u=0.98, temp=100, iterations=10, move="swap", op="normal"):
+    schedule_lenght = len(schdl.joblist)
+    steps = []
+    for i in range(iterations):
+        tmp_schdl = schdl.__copy__()
+
+        if move == "insert":
+            random_insert(tmp_schdl, schedule_lenght)
+        else:
+            swap(tmp_schdl, schedule_lenght)
+
+        # Nowe rozwiazanie lepsze od obecnego
+        if tmp_schdl.cmax() < schdl.cmax():
+            schdl = tmp_schdl
+
+        # Nowe rozwiazanie gorsze od obecnego
+        elif tmp_schdl.cmax() > schdl.cmax():
+            prob = math.exp((schdl.cmax() - tmp_schdl.cmax())/temp)
+            if random() < prob:
+                schdl = tmp_schdl
+                
+        else:
+            continue
+            
+        steps.append(schdl.cmax())
+        if op == "alternative":
+            temp = temp * (i+1)/iterations
+        else:
+            temp = u * temp
 
     return int(schdl.cmax()), steps
