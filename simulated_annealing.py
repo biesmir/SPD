9f9e60001dcfb4566224f7c0a69832eb64a3c24d
Jakub Cebulski <235773@student.pwr.edu.pl>
2019-04-12 11:36:33 +0200

    wykresy
    dodanie wykresów, powiększenie istniejących, dodanie legendy

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -19,12 +19,12 @@ def swap(schdl, schedule_length):
 def random_insert(schdl, schedule_length):
     # funkcja może mieć problemy jeśli dostanie harmoonogram krótszy niż 3 zadania
     job_index = randint(0, schedule_length - 1)
-    new_index = randint(0, schedule_length - 2)
+    new_index = randint(0, schedule_length - 1)
 
     # sprawdzamy czy nie wylosowało się dwa razy to samo
     while job_index == new_index:
         job_index = randint(0, schedule_length - 1)
-        new_index = randint(0, schedule_length - 2)
+        new_index = randint(0, schedule_length - 1)
 
     job = schdl.joblist[job_index]
     del schdl.joblist[job_index]
@@ -71,11 +71,16 @@ def annealing_prob(schdl, u=0.98, temp=100, iterations=10, move="swap", op="norm
             random_insert(tmp_schdl, schedule_lenght)
         else:
             swap(tmp_schdl, schedule_lenght)
-        
+                
+        if tmp_schdl.cmax() < schdl.cmax():
+            prob = 0.98
+            if random() < prob:
+                schdl = tmp_schdl
         # Odrzucenie prawdopodobienstwa 1 dla nowych lepszych rozwiazan
-        prob = math.exp((schdl.cmax() - tmp_schdl.cmax())/temp)
-        if random() < prob:
-            schdl = tmp_schdl
+        else:
+            prob = math.exp((schdl.cmax() - tmp_schdl.cmax())/temp)
+            if random() < prob:
+                schdl = tmp_schdl
 
         steps.append(schdl.cmax())
         if op == "alternative":
