fac6043393de2dcd61260ce021a2f678af50824f
Jakub Cebulski <235773@student.pwr.edu.pl>
2019-05-10 02:10:11 +0200

    naprawa działania kopców

--- heap.py
+++ heap.py
@@ -13,36 +13,32 @@ class HeapQ:
         if not self.array:
             self.array.append(job)
         else:
-            self.size = len(self.array)
             self.array.append(job)
-            index = self.size
+            self.size = len(self.array)
+            index = self.size - 1
             parent = int((index - 1) / 2)
             while index != 0 and self.array[parent].q < self.array[index].q:
 
                 self.array[parent], self.array[index] = self.array[index], self.array[parent]
+                index = parent
                 parent = int((index - 1) / 2)
 
     def pop(self):
         value = self.array[0]
-
-        del self.array[0]
-        self.size = len(self.array)
+        self.array[0] = self.array[-1]
+        del self.array[-1]
+        self.size = len(self.array) - 1
         index = 0
-        while index < self.size and index * 2 + 1 < self.size:
-            if self.array[index].q < self.array[index * 2 + 1].q:
-                self.array[index * 2 + 1], self.array[index] = self.array[index], self.array[index * 2 + 1]
-                index = index * 2 + 1
-
-            elif index * 2 + 2 < self.size:
-                if self.array[index].q < self.array[index * 2 + 2].q:
-                    self.array[index * 2 + 2], self.array[index] = self.array[index], self.array[index * 2 + 2]
-                    index = index * 2 + 2
-
-                else:
-                    break
-
+        while index*2 + 1 < self.size:
+            if self.array[index * 2 + 2].q > self.array[index * 2 + 1].q:
+                bigger = index * 2 + 2
             else:
+                bigger = index * 2 + 1
+            if self.array[index].q > self.array[bigger].q:
                 break
+            else:
+                self.array[bigger], self.array[index] = self.array[index], self.array[bigger]
+                index = bigger
 
         return value
 
@@ -62,34 +58,31 @@ class HeapR:
         if not self.array:
             self.array.append(job)
         else:
-            self.size = len(self.array)
             self.array.append(job)
-            index = self.size
+            self.size = len(self.array)
+            index = self.size - 1
             parent = int((index - 1) / 2)
             while index != 0 and self.array[parent].r > self.array[index].r:
 
                 self.array[parent], self.array[index] = self.array[index], self.array[parent]
+                index = parent
                 parent = int((index - 1) / 2)
 
     def pop(self):
         value = self.array[0]
-        del self.array[0]
-        self.size = len(self.array)
+        self.array[0] = self.array[-1]
+        del self.array[-1]
+        self.size = len(self.array) - 1
         index = 0
-        while index < self.size * index*2 + 1 < self.size:
-            if self.array[index].r > self.array[index * 2 + 1].r:
-                self.array[index * 2 + 1], self.array[index] = self.array[index], self.array[index * 2 + 1]
-                index = index * 2 + 1
-
-            elif index*2 + 2 < self.size:
-                if self.array[index].r > self.array[index * 2 + 2].r:
-                    self.array[index * 2 + 2], self.array[index] = self.array[index], self.array[index * 2 + 2]
-                    index = index * 2 + 2
-                else:
-                    break
-
+        while index*2 + 1 < self.size:
+            if self.array[index * 2 + 2].r < self.array[index * 2 + 1].r:
+                smaller = index * 2 + 2
             else:
+                smaller = index * 2 + 1
+            if self.array[index].r < self.array[smaller].r:
                 break
+            else:
+                self.array[smaller], self.array[index] = self.array[index], self.array[smaller]
+                index = smaller
 
         return value
-
