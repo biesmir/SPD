fd79c02f9b7dae37198a74137752fc750e9f3db1
Jakub Cebulski <235773@student.pwr.edu.pl>
2019-06-07 13:37:55 +0200

    działające cp i milp dla pd

--- ortools_wt.py
+++ ortools_wt.py
@@ -13,25 +13,59 @@ class PWD():
         self.D = d
 
 
+def CP_WT(jobs, instanceName):
+    model = cp_model.CpModel()
+    solver = cp_model.CpSolver()
+    sumaP = [job.P for job in jobs]
+    variableMaxValue = sum(job.W*(sum(sumaP)-job.D) if sum(sumaP)-job.D > 0 else 0 for job in jobs)
+
+    starts = [(model.NewIntVar(0, variableMaxValue, "starts"+str(i))) for i in range(len(jobs))]
+    penalty = [(model.NewIntVar(0, variableMaxValue, "kara" + str(i))) for i in range(len(jobs))]
+
+    alfasMatrix = {}
+    for i in range(len(jobs)):
+        for j in range(len(jobs)):
+            alfasMatrix[i, j] = model.NewIntVar(0, 1, "alfa"+str(i) + "_" + str(j))
+
+    for i in range(len(jobs)):
+        for j in range(i+1, len(jobs)):
+            model.Add(starts[i]+jobs[i].P <= starts[j] + alfasMatrix [i,j]*variableMaxValue)
+            model.Add(starts[j]+jobs[j].P <= starts[i] + alfasMatrix[j,i] *variableMaxValue)
+            model.Add(alfasMatrix[i,j] + alfasMatrix[j,i] == 1)
+
+    model.Minimize(sum((starts[i]+jobs[i].P-jobs[i].D)*jobs[i].W if (starts[i]+jobs[i].P-jobs[i].D) >= 0 else 0 for i in range(len(starts))))
+    status = solver.Solve(model)
+    pi = [(i, start.GetVarValueMap()) for start in starts]
+    print(instanceName, "Suma opt:", solver.ObjectiveValue())
+
+    pi.sort(key=lambda x: x[1])
+    print(pi)
+
+
 def Milp_WT(jobs, instanceName):
+    solver = pywraplp.Solver('simple_mip_program',
+            pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
     sumaP = []
     for x in range(len(jobs)):
         sumaP.append(jobs[x].P)
     variableMaxValue = sum(job.W*(sum(sumaP)-job.D) if sum(sumaP)-job.D > 0 else 0 for job in jobs)
-    solver = pywraplp.Solver('simple_mip_program',
-            pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
 
     starts = [(solver.IntVar(0, variableMaxValue, "starts"+str(i))) for i in range(len(jobs))]
-    penalty = [(solver.IntVar(0, variableMaxValue, "kara"+str(i))) for i in range(len(jobs))]
-    suma_wT = solver.IntVar(0, variableMaxValue, "suma_wT")
+    penalty = [(solver.IntVar(0, variableMaxValue, "kara" + str(i))) for i in range(len(jobs))]
 
-    for i in range(1, len(jobs)):
-        solver.Add(starts[i] >= starts[i-1]+jobs[i-1].P)
-        solver.Add(penalty[i] >= (starts[i] + jobs[i].P - jobs[i].D) * jobs[i].W if (starts[i] + jobs[i].P - jobs[i].D) >= 0 else 0)
-    solver.Add(suma_wT >= sum(penalty))
-    # solver.Add(suma_wT >= sum((starts[i] + jobs[i].P - jobs[i].D) * jobs[i].W if (starts[i] + jobs[i].P - jobs[i].D) >= 0 else 0 for i in range(len(jobs))))
+    alfasMatrix = {}
+    for i in range(len(jobs)):
+        for j in range(len(jobs)):
+            alfasMatrix[i, j] = solver.IntVar(0, 1, "alfa"+str(i) + "_" + str(j))
 
-    solver.Minimize(suma_wT)
+    for i in range(len(jobs)):
+        for j in range(i+1, len(jobs)):
+            solver.Add(starts[i]+jobs[i].P <= starts[j] + alfasMatrix [i,j]*variableMaxValue)
+            solver.Add(starts[j]+jobs[j].P <= starts[i] + alfasMatrix[j,i] *variableMaxValue)
+            solver.Add(alfasMatrix[i,j] + alfasMatrix[j,i] == 1)
+        solver.Add(penalty[i] >= (starts[i]+jobs[i].P-jobs[i].D)*jobs[i].W)
+
+    solver.Minimize(sum(penalty))
     status = solver.Solve()
     if status != pywraplp.Solver.OPTIMAL:
         print("Not optimal!")
@@ -59,7 +93,8 @@ def GetPWDsFromFile (file_path):
 
 
 if __name__ == '__main__':
-    files = ["./dane pwd/data1.txt"]
+    files = ["./dane pwd/data11.txt"]
     for file in files:
         jobs = GetPWDsFromFile(file)
+        # CP_WT(jobs, file)
         Milp_WT(jobs, file)
